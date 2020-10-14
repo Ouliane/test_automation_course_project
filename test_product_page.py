12a9c9aeb499bed285628dee9445382a5733c3f4
Ouliane <63844610+Ouliane@users.noreply.github.com>
2020-10-14 11:36:36 +0300

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -81,7 +81,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
-    @pytest.mark.need_review2
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, link)
         page.open()
